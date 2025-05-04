32f6ea6a97f5b326c67c0d915539e0206d2ee907
aafanasev <tafanasyev@gmail.com>
2025-05-04 00:07:00 -0700

    support landscape mode

--- app/src/main/java/net/afanasev/otonfm/player/PlayerViewScreen.kt
+++ app/src/main/java/net/afanasev/otonfm/player/PlayerViewScreen.kt
@@ -1,11 +1,16 @@
 package net.afanasev.otonfm.player
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -14,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.media3.common.MediaMetadata
@@ -29,6 +35,7 @@ fun PlayerViewScreen(
     modifier: Modifier = Modifier,
 ) {
     val controller = viewModel.controller
+    val configuration = LocalConfiguration.current
 
     var isPlaying by remember { mutableStateOf(false) }
     var isChangingState by remember { mutableStateOf(false) }
@@ -58,36 +65,70 @@ fun PlayerViewScreen(
         }
     }
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
+    val onPlayButtonClick = {
+        isChangingState = true
+        viewModel.playPause()
+    }
 
-        Logo(modifier = Modifier.fillMaxWidth(0.6f))
-
-        Spacer(modifier = Modifier.height(36.dp))
-
-        Artwork(
-            artworkUri = artwork.value,
-            modifier = Modifier.fillMaxWidth(0.8f),
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Title(
-            text = title,
-            modifier = Modifier.fillMaxWidth(0.8f),
-        )
-
-        Spacer(modifier = Modifier.height(36.dp))
-
-        PlayButton(
-            isPlaying = isPlaying,
-            isEnabled = !isChangingState,
+    if (configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        Row(
+            modifier = modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            isChangingState = true
-            viewModel.playPause()
+            Box(
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(20.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                Artwork(
+                    artworkUri = artwork.value,
+                    modifier = Modifier.fillMaxHeight(0.8f),
+                )
+            }
+            Column(
+                modifier = Modifier.weight(3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Logo(modifier = Modifier.fillMaxWidth(0.6f))
+                Spacer(modifier = Modifier.height(24.dp))
+                Title(
+                    text = title,
+                    modifier = Modifier.fillMaxWidth(0.8f),
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                PlayButton(
+                    isPlaying = isPlaying,
+                    isEnabled = !isChangingState,
+                    onClick = onPlayButtonClick,
+                )
+            }
+        }
+    } else {
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Logo(modifier = Modifier.fillMaxWidth(0.6f))
+            Spacer(modifier = Modifier.height(36.dp))
+            Artwork(
+                artworkUri = artwork.value,
+                modifier = Modifier.fillMaxWidth(0.8f),
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            Title(
+                text = title,
+                modifier = Modifier.fillMaxWidth(0.8f),
+            )
+            Spacer(modifier = Modifier.height(36.dp))
+            PlayButton(
+                isPlaying = isPlaying,
+                isEnabled = !isChangingState,
+                onClick = onPlayButtonClick,
+            )
         }
     }
 }

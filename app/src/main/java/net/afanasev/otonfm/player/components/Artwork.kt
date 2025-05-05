7518ae6258d47f47bae2e1b4b821aa7059945e56
aafanasev <tafanasyev@gmail.com>
2025-05-04 23:10:21 -0700

    rework artwork

--- app/src/main/java/net/afanasev/otonfm/player/components/Artwork.kt
+++ app/src/main/java/net/afanasev/otonfm/player/components/Artwork.kt
@@ -1,25 +1,27 @@
 package net.afanasev.otonfm.player.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Audiotrack
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import net.afanasev.otonfm.R
 import net.afanasev.otonfm.ui.theme.LocalCustomColorsPalette
 
 @Composable
@@ -45,12 +47,26 @@ fun Artwork(
             .background(LocalCustomColorsPalette.current.previewBackground),
     ) {
         if (artworkUri == null) {
-            Icon(
-                imageVector = Icons.Filled.Audiotrack,
-                contentDescription = "Preview",
-                tint = Color.LightGray,
-                modifier = Modifier.size(96.dp),
-            )
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(R.drawable.note),
+                    contentDescription = "Musical note",
+                    colorFilter = ColorFilter.tint(Color.LightGray),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .fillMaxSize(.5f)
+                        .offset(x = (-10).dp, y = (-10).dp),
+                )
+                Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = "App logo",
+                    colorFilter = ColorFilter.tint(Color.LightGray),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .fillMaxWidth(0.5f)
+                        .padding(bottom = 40.dp),
+                )
+            }
         } else {
             AsyncImage(
                 model = artworkUri,

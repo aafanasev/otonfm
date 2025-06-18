53a9a5569590a4756678d25025fef60aba0da8b2
aafanasev <tafanasyev@gmail.com>
2025-06-17 23:19:04 -0700

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "net.afanasev.otonfm"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

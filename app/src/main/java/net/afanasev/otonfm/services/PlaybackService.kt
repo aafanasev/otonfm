286a1e6c2d39846641ef8670357f0c6be511b12a
aafanasev <tafanasyev@gmail.com>
2025-07-13 22:45:39 -0700

    stop playing when unplug headphones

--- app/src/main/java/net/afanasev/otonfm/services/PlaybackService.kt
+++ app/src/main/java/net/afanasev/otonfm/services/PlaybackService.kt
@@ -1,5 +1,10 @@
 package net.afanasev.otonfm.services
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.AudioManager
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.exoplayer.ExoPlayer
@@ -9,13 +14,36 @@ import androidx.media3.session.MediaSessionService
 class PlaybackService : MediaSessionService() {
 
     private var mediaSession: MediaSession? = null
+    private val noisyReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            // stop playing music when become noisy (e.g. unplug headphones)
+            if (intent.action == AudioManager.ACTION_AUDIO_BECOMING_NOISY) {
+                mediaSession?.let {
+                    if (it.player.isPlaying) {
+                        it.player.stop()
+                    }
+                }
+            }
+        }
+
+    }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
         mediaSession
 
     override fun onCreate() {
         super.onCreate()
+        initMediaSession()
+        registerReceiver(noisyReceiver, IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY))
+    }
 
+    override fun onDestroy() {
+        destroyMediaSession()
+        unregisterReceiver(noisyReceiver)
+        super.onDestroy()
+    }
+
+    private fun initMediaSession() {
         val player = ExoPlayer.Builder(this)
             .setAudioAttributes(
                 AudioAttributes.Builder()
@@ -28,12 +56,11 @@ class PlaybackService : MediaSessionService() {
         mediaSession = MediaSession.Builder(this, player).build()
     }
 
-    override fun onDestroy() {
+    private fun destroyMediaSession() {
         mediaSession?.let {
             it.player.release()
             it.release()
             mediaSession = null
         }
-        super.onDestroy()
     }
 }

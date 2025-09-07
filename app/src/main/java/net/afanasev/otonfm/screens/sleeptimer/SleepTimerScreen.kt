971baa9f346221c21f094409a0b4447f6e4ecdd2
aafanasev <tafanasyev@gmail.com>
2025-09-07 00:47:48 -0700

    completed a working prototype of the feature

--- app/src/main/java/net/afanasev/otonfm/screens/sleeptimer/SleepTimerScreen.kt
+++ app/src/main/java/net/afanasev/otonfm/screens/sleeptimer/SleepTimerScreen.kt
@@ -1,12 +1,112 @@
 package net.afanasev.otonfm.screens.sleeptimer
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.SystemClock
+import android.provider.Settings
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.net.toUri
+import net.afanasev.otonfm.R
+import net.afanasev.otonfm.log.Logger
+import net.afanasev.otonfm.services.PlaybackService
 import net.afanasev.otonfm.ui.components.Dialog
+import net.afanasev.otonfm.ui.components.DialogItem
+import java.util.concurrent.TimeUnit
 
 @Composable
 fun SleepTimerScreen() {
+    val context = LocalContext.current
+
     Dialog {
 
+        if (true) {
+            Text(text = "Sleep timer is on")
+        }
+
+        DialogItem(
+            R.string.sleep_timer_off,
+            onClick = {
+
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_10min,
+            onClick = {
+                val am = context.getSystemService(AlarmManager::class.java)
+                val intent =
+                    Intent(PlaybackService.ACTION_SLEEP_TIMER).setPackage(context.packageName)
+                val pi = PendingIntent.getBroadcast(
+                    context,
+                    1001,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+                )
+                val triggerAt = SystemClock.elapsedRealtime() + TimeUnit.MINUTES.toMillis(1)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    if (am.canScheduleExactAlarms()) {
+                        println("asdasd schedule")
+                        am.setExactAndAllowWhileIdle(
+                            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            triggerAt,
+                            pi
+                        )
+                    } else {
+                        // TODO: request
+                        println("asdasd need to request")
+
+                        val requestPermission =
+                            Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                                data = "package:${context.packageName}".toUri()
+                            }
+                        context.startActivity(requestPermission)
+                    }
+                } else {
+                    println("asdasd schedule 2")
+                    am.setExactAndAllowWhileIdle(
+                        AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        triggerAt,
+                        pi
+                    )
+                }
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_15min,
+            onClick = {
+
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_20min,
+            onClick = {
+
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_30min,
+            onClick = {
+
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_45min,
+            onClick = {
+
+            },
+        )
+        DialogItem(
+            R.string.sleep_timer_60min,
+            onClick = {
+
+            },
+        )
     }
 
 }
--- app/src/main/java/net/afanasev/otonfm/services/PlaybackService.kt
+++ app/src/main/java/net/afanasev/otonfm/services/PlaybackService.kt
@@ -1,32 +1,24 @@
 package net.afanasev.otonfm.services
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.media.AudioManager
+import android.os.Build
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import net.afanasev.otonfm.BuildConfig
 
 class PlaybackService : MediaSessionService() {
 
     private var mediaSession: MediaSession? = null
-    private val noisyReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            // stop playing music when become noisy (e.g. unplug headphones)
-            if (intent.action == AudioManager.ACTION_AUDIO_BECOMING_NOISY) {
-                mediaSession?.let {
-                    if (it.player.isPlaying) {
-                        it.player.stop()
-                    }
-                }
-            }
-        }
-
-    }
+    private val noisyReceiver = StopCommandReceiver(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+    private val sleepTimerReceiver = StopCommandReceiver(ACTION_SLEEP_TIMER)
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
         mediaSession
@@ -35,12 +27,25 @@ class PlaybackService : MediaSessionService() {
         super.onCreate()
         initMediaSession()
         registerReceiver(noisyReceiver, IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY))
+        registerSleepTimerReceiver()
     }
 
     override fun onDestroy() {
         destroyMediaSession()
         unregisterReceiver(noisyReceiver)
+        unregisterReceiver(sleepTimerReceiver)
         super.onDestroy()
+    }
+
+
+    private fun registerSleepTimerReceiver() {
+        val filter = IntentFilter(ACTION_SLEEP_TIMER)
+        if (Build.VERSION.SDK_INT >= 33) {
+            registerReceiver(sleepTimerReceiver, filter, RECEIVER_NOT_EXPORTED)
+        } else {
+            @SuppressLint("UnspecifiedRegisterReceiverFlag")
+            registerReceiver(sleepTimerReceiver, filter)
+        }
     }
 
     private fun initMediaSession() {
@@ -63,4 +68,21 @@ class PlaybackService : MediaSessionService() {
             mediaSession = null
         }
     }
+
+    inner class StopCommandReceiver(private val action: String) : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == action) {
+                mediaSession?.let {
+                    if (it.player.isPlaying) {
+                        it.player.stop()
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val ACTION_SLEEP_TIMER = "${BuildConfig.APPLICATION_ID}.ACTION_SLEEP_TIMER"
+    }
+
 }
